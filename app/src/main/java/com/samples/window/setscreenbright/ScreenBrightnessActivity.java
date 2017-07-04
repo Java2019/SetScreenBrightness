12609b63ed453f6344ee1620b4636c5bd577f20c
java2019 <java_2019@mail.ru>
2017-07-04 17:03:49 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/window/setscreenbright/ScreenBrightnessActivity.java
@@ -0,0 +1,13 @@
+package com.samples.window.setscreenbright;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenBrightnessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_brightness);
+    }
+}
