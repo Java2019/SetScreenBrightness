5de08e8dae1d795e4a0c60a7680053588b7625ae
java2019 <java_2019@mail.ru>
2017-07-05 17:06:42 +0300

    init2

--- app/src/main/java/com/samples/window/setscreenbright/ScreenBrightnessActivity.java
+++ app/src/main/java/com/samples/window/setscreenbright/ScreenBrightnessActivity.java
@@ -3,6 +3,7 @@ package com.samples.window.setscreenbright;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -37,16 +38,36 @@ public class ScreenBrightnessActivity extends AppCompatActivity
             Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
         }
 
+        int percBrigh = brightness * 100 / 255;
+        seekBar.setProgress(percBrigh);
+
+        // Выводим в текстовое поле значение яркости экрана в %
+        text.setText(percBrigh + "%");
+
+        seekBar.setOnSeekBarChangeListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        // Cохраняем значение яркости экрана в системных настройках
+        android.provider.Settings.System.putInt(
+                getContentResolver(),
+                android.provider.Settings.System.SCREEN_BRIGHTNESS,
+                brightness);
     }
 
     @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
+    public void onProgressChanged(SeekBar seekBar, int arg1, boolean b) {
+        text.setText(arg1 + "%");
+        // Получаем параметр, определяющий яркость экрана
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        // Устанавливаем значение поля screenBrightness,
+        // преобразуя значение яркости в % (0...100) в диапазон
+        // значений поля screenBrightness (0...1)
+        params.screenBrightness = (float)arg1 / 100;
+        // Устанавливаем яркость экрана
+        getWindow().setAttributes(params);
+        brightness = arg1 * 255 / 100;
     }
 
     @Override
