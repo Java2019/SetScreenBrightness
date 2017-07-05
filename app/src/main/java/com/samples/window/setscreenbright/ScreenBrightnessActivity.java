552966241549a70902bb4ace0ff86ccaea99f3bb
java2019 <java_2019@mail.ru>
2017-07-05 13:06:04 +0300

    test1

--- app/src/main/java/com/samples/window/setscreenbright/ScreenBrightnessActivity.java
+++ app/src/main/java/com/samples/window/setscreenbright/ScreenBrightnessActivity.java
@@ -9,5 +9,6 @@ public class ScreenBrightnessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_brightness);
+
     }
 }
